85ba5557fd66663dc8751c138129a49c1f42831a
Matthieu DESILE <matthieu.desile@gmail.com>
2022-06-03 23:53:17 +0200

    ajout de symfony/test-pack

--- src/Kernel.php
+++ src/Kernel.php
@@ -8,4 +8,12 @@ use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
+
+    /**
+     * Gets the path to the data directory.
+     */
+    public function getStorageDir(): string
+    {
+        return $this->getProjectDir() . '/var/storage/' . $this->environment;
+    }
 }
