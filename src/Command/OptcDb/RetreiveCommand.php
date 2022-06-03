cc1d02375abc11fb07755b45e97a3ec0f27113ce
Matthieu DESILE <matthieu.desile@gmail.com>
2022-06-04 00:21:24 +0200

    ajout commande pour récupérer les fichiers d'optc-db

---
+++ src/Command/OptcDb/RetreiveCommand.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Command\OptcDb;
+
+use App\Kernel;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+#[AsCommand(
+    name: 'optc-db:retreive',
+    description: 'retrieves raw js files from the optc-db project on GH'
+)]
+class RetreiveCommand 
+    extends Command
+{
+    private string $storage_dir;
+
+    public function __construct(Kernel $kernel, private Filesystem $fs, private HttpClientInterface $httpClient)
+    {
+        parent::__construct();
+        $this->storage_dir = $kernel->getStorageDir();        
+    }
+
+    protected function configure()
+    {
+        // ...
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+		$base = 'https://raw.githubusercontent.com/optc-db/optc-db.github.io/master';
+		$base = 'https://optc-db.github.io/';
+        $url = array(
+            'units'      => "{$base}/common/data/units.js",
+            'details'    => "{$base}/common/data/details.js",
+            'evolutions' => "{$base}/common/data/evolutions.js",
+            'cooldowns'  => "{$base}/common/data/cooldowns.js",
+            'families'   => "{$base}/common/data/families.js",
+            'drops'      => "{$base}/common/data/drops.js",
+            'flags'      => "{$base}/common/data/flags.js",
+			'rumble'     => "{$base}/common/data/rumble.json",
+            'utils'      => "{$base}/common/js/utils.js",
+        );
+
+        $dest_dir = "{$this->storage_dir}/optc-db";
+        if(! $this->fs->exists($dest_dir)){
+            mkdir($dest_dir, 0777, true);
+        }
+
+        foreach ($url as $key => $value) {
+	        $ext = pathinfo($value, PATHINFO_EXTENSION);
+            $response = $this->httpClient->request('GET', $value);
+            $content = $response->getContent();
+            file_put_contents("{$dest_dir}/raw-{$key}.{$ext}", $content);
+        }
+
+        return self::SUCCESS;
+    }
+}
---
+++ tests/Command/OptcDb/RetreiveCommandTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Tests\Command\OptcDb;
+
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Component\Console\Tester\CommandTester;
+
+class RetreiveCommandTest extends KernelTestCase
+{
+    public function testExecute()
+    {
+        $kernel = self::bootKernel();
+        $application = new Application($kernel);
+
+        $command = $application->find('optc-db:retreive');
+        $commandTester = new CommandTester($command);
+        $commandTester->execute([]);
+
+        $commandTester->assertCommandIsSuccessful();
+
+        // the output of the command in the console
+        $output = $commandTester->getDisplay();
+    }
+}
